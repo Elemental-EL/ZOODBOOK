1b2cdc3d2c83f24fced1977d2d2cf8a5424a013c
Elemental-EL <elyar.k@yahoo.com>
2024-01-12 10:47:21 +0330

    update-5

---
+++ src/main/java/org/example/zoodbook/EditCredsController.java
@@ -0,0 +1,44 @@
+package org.example.zoodbook;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class EditCredsController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    private ImageView logobtn, backbtn;
+    @FXML
+    private TextField userText, passwordText, repPasswordText;
+    @FXML
+    private Button savebtn;
+    public void onBackClicked(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Profile.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void onLogoClicked(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void onSaveClicked (ActionEvent event){
+
+    }
+}
