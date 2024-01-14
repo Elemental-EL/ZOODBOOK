1d6fd7b748b29896212e91058882f898c4af64ab
hamed005 <hamednobakht949@gmail.com>
2024-01-14 16:36:20 +0330

    update-4-1

---
+++ src/main/java/org/example/zoodbook/OrdersController.java
@@ -0,0 +1,17 @@
+package org.example.zoodbook;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class OrdersController {
+    @FXML
+    private ImageView logobtn;
+    @FXML
+    void onLogoClicked (MouseEvent event){
+
+    }
+
+    public void onBackClicked(MouseEvent event) {
+    }
+}
