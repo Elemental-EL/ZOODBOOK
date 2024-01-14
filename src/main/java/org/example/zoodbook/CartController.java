a28ada26fd06c2ae228cde620096a32708e1372e
hamed005 <hamednobakht949@gmail.com>
2024-01-14 23:11:53 +0330

    update-4-2

---
+++ src/main/java/org/example/zoodbook/CartController.java
@@ -0,0 +1,15 @@
+package org.example.zoodbook;
+
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
+public class CartController {
+    public void onAddClicked(ActionEvent actionEvent) {
+    }
+
+    public void onMinusClicked(ActionEvent actionEvent) {
+    }
+
+    public void onRicycleBinClicked(MouseEvent event) {
+    }
+}
--- src/main/java/org/example/zoodbook/SectionController.java
+++ src/main/java/org/example/zoodbook/SectionController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class SectionController {
     @FXML
@@ -16,6 +17,10 @@ public class SectionController {
     }
     @FXML
     void onOrdersClicked (ActionEvent event){
+
+    }
+    @FXML
+    void onLogoClicked(MouseEvent event) {
 
     }
 }
