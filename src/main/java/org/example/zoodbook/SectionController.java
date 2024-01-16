38324629d21acd2c62c010c1f85c1a73cf473ea5
Elemental-EL <elyar.k@yahoo.com>
2024-01-16 10:26:16 +0330

    update-8/Getting in the Back door XD

--- src/main/java/org/example/zoodbook/SectionController.java
+++ src/main/java/org/example/zoodbook/SectionController.java
@@ -2,25 +2,47 @@ package org.example.zoodbook;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SectionController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
     @FXML
     private ImageView logobtn;
     @FXML
     private Button storagebtn, ordersbtn;
     @FXML
-    void onStorageClicked (ActionEvent event){
-
+    void onStorageClicked (ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Storage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
     @FXML
-    void onOrdersClicked (ActionEvent event){
-
+    void onOrdersClicked (ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Orders.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
     @FXML
-    void onLogoClicked(MouseEvent event) {
-
+    void onLogoClicked(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
--- src/main/java/org/example/zoodbook/StorageController.java
+++ src/main/java/org/example/zoodbook/StorageController.java
@@ -2,18 +2,37 @@ package org.example.zoodbook;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class StorageController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
     @FXML
     private ImageView logobtn;
     @FXML
-    void onLogoClicked (MouseEvent event){
-
+    void onLogoClicked (MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
-    public void onBackClicked(MouseEvent event) {
+    public void onBackClicked(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Section.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     public void onEdit1(ActionEvent actionEvent) {
