48a4349c1666bd55f594518ce6030880f31e11dc
Elemental-EL <elyar.k@yahoo.com>
2024-01-17 13:04:01 +0330

    update-8.8/Sign in done!!!

--- src/main/java/org/example/zoodbook/SignInController.java
+++ src/main/java/org/example/zoodbook/SignInController.java
@@ -13,6 +13,8 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Objects;
 
@@ -20,6 +22,7 @@ public class SignInController {
     private Stage stage;
     private Scene scene;
     private Parent root;
+    public int loggedInUserId=100;
     @FXML
     private ImageView backbtn,logobtn;
     @FXML
@@ -51,6 +54,7 @@ public class SignInController {
     }
     @FXML
     void onLoginClicked(ActionEvent event) throws IOException {
+        errorText.setText("");
         if (usernameText.getText().trim().isEmpty()||passwordText.getText().trim().isEmpty()){
             errorText.setText("*پر کردن تمامی فیلد ها الزامی است.");
         }
@@ -60,6 +64,33 @@ public class SignInController {
             scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
+        } else {
+            String entryUsername,line = null;
+            int entryPassword;
+            boolean Authentication = false;
+            entryUsername = usernameText.getText();
+            entryPassword = passwordText.getText().hashCode();
+            BufferedReader reader = new BufferedReader(new FileReader("Files/Users.txt"));
+            while (!Authentication&&((line=reader.readLine())!=null)){
+                if (Objects.equals(entryUsername, line.split("#")[4])){
+                    if (entryPassword==Integer.parseInt(line.split("#")[5])){
+                        Authentication = true;
+                    }
+                }
+            }
+            reader.close();
+            if (Authentication){
+                loggedInUserId = Integer.parseInt(line.split("#")[1]);
+                Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+                stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+                scene = new Scene(root);
+                stage.setScene(scene);
+                stage.setResizable(false);
+                stage.show();
+            }
+            else {
+                errorText.setText("*نام کاربری یا رمز عبور اشتباه است.");
+            }
         }
     }
 }
