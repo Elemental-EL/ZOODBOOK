7137867ccf3ec65a1821eb18cb545a94f193a217
hamed005 <hamednobakht949@gmail.com>
2024-01-27 00:53:28 +0330

    update-9.54 / CartController bug fix

--- src/main/java/org/example/zoodbook/CartController.java
+++ src/main/java/org/example/zoodbook/CartController.java
@@ -38,34 +38,27 @@ public class CartController {
         String[] orderBooksQuantity = new String[48];
         String[] orderBooksName= new String[48];
         String[] orderBooksPrice = new String[48];
-        if (SignInController.loggedInUserId!=100){
+        if (SignInController.loggedInUserId!=100) {
             BufferedReader reader = new BufferedReader(new FileReader("Files/Cart.txt"));
             String read;
-            while (((read = reader.readLine()) != null) && (!Objects.equals(read.split("#")[1], String.valueOf(SignInController.loggedInUserId)))){
+            while (((read = reader.readLine()) != null) && (!Objects.equals(read.split("#")[1], String.valueOf(SignInController.loggedInUserId)))) {
             }
-            for (int i = 0; i<(read.split("#").length-1)/2 ; i++){
-                int k = (2*i)+2;
+            for (int i = 0; i < (read.split("#").length - 1) / 2; i++) {
+                int k = (2 * i) + 2;
                 orderBooksID[i] = read.split("#")[k];
-                orderBooksQuantity[i] = read.split("#")[k+1];
+                orderBooksQuantity[i] = read.split("#")[k + 1];
             }
             reader.close();
-            for (int i = 0 ; i<48 && orderBooksID[i] != null ; i++){
+            for (int i = 0; i < 48 && orderBooksID[i] != null; i++) {
                 BufferedReader reader1 = new BufferedReader(new FileReader("Files/Books.txt"));
                 String line1;
-                while (((line1 = reader1.readLine())!=null) && (!Objects.equals(line1.split("#")[1], orderBooksID[i]))){
+                while (((line1 = reader1.readLine()) != null) && (!Objects.equals(line1.split("#")[1], orderBooksID[i]))) {
                 }
                 orderBooksName[i] = line1.split("#")[4];
                 orderBooksPrice[i] = line1.split("#")[3];
             }
-            newOrder(orderBooksName , orderBooksQuantity , orderBooksPrice , orderBooksID);
-        }else {
+            newOrder(orderBooksName, orderBooksQuantity, orderBooksPrice, orderBooksID);
         }
-//        BufferedReader reader1 = new BufferedReader(new FileReader("Files/Cart.txt"));
-//        StringBuilder line = new StringBuilder();
-//        String line2;
-//        while (((line2 = reader1.readLine())!=null) && (!Objects.equals(line2.split("#")[1], String.valueOf(SignInController.loggedInUserId)))){
-//            line.append(line2);
-//        }
     }
 
     public void onAddClicked(ActionEvent actionEvent) {
@@ -95,7 +88,6 @@ public class CartController {
     }
     public void onContinueShoppingClicked(ActionEvent actionEvent) {
         setOrdersVBox(ordersVBox);
-
     }
     public void setOrdersVBox(VBox ordersVBox) {
         ordersVBox.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
@@ -106,17 +98,17 @@ public class CartController {
         //Get from file
         return userOrders;
     }
-    public void newOrder(String[] a , String[] b , String[] c , String[] d){
-        for (int i=0 ; i<48 && a[i] != null ; i++){
+    public void newOrder(String[] orderBooksName , String[] orderBooksQuantity , String[] orderBooksPrice , String[] orderBooksID) throws IOException {
+        for (int i=0 ; i<48 && orderBooksName[i] != null ; i++){
             BorderPane newPane =  new BorderPane();
             Label bookName = new Label("نام کتاب :");
             Label quantity = new Label("تعداد :");
             Label bookPrice = new Label("قیمت :");
-            TextField orderBookName = new TextField(a[i]);
+            TextField orderBookName = new TextField(orderBooksName[i]);
             orderBookName.setEditable(false);
-            TextField orderBookQuantity = new TextField(b[i]);
+            TextField orderBookQuantity = new TextField(orderBooksQuantity[i]);
             orderBookQuantity.setEditable(false);
-            TextField orderBookPrice = new TextField(String.valueOf(Long.parseLong(b[i])*Long.parseLong(c[i])));
+            TextField orderBookPrice = new TextField(String.valueOf(Long.parseLong(orderBooksQuantity[i])*Long.parseLong(orderBooksPrice[i])));
             orderBookPrice.setEditable(false);
             Button add = new Button("+");
             Button reduce = new Button("-");
@@ -147,10 +139,10 @@ public class CartController {
             add.setOnAction(new EventHandler<ActionEvent>() {
                 @Override
                 public void handle(ActionEvent actionEvent) {
-                    number = Long.valueOf(b[finalI]);
+                    number = Long.valueOf(orderBooksQuantity[finalI]);
                     number++;
                     orderBookQuantity.setText(String.valueOf(number));
-                    b[finalI] = String.valueOf(number);
+                    orderBooksQuantity[finalI] = String.valueOf(number);
                     try {
                         BufferedReader reader = new BufferedReader(new FileReader("Files/Cart.txt"));
                         StringBuilder lines = new StringBuilder();
@@ -159,8 +151,8 @@ public class CartController {
                         while (((line = reader.readLine())!=null) && (!String.valueOf(SignInController.loggedInUserId).equals(line.split("#")[1]))){
                             lines.append(line + "\n");
                         }
-                        for (int j=0 ; j<(line.split("#").length-1)/2 ; j++){
-                            line1.append(d[j]+ "#" + b[j] + "#");
+                        for (int j=0 ; j<(line.split("#").length-2)/2 ; j++){
+                            line1.append(orderBooksID[j]+ "#" + orderBooksQuantity[j] + "#");
                         }
                         lines.append(line1 + "\n");
                         while ((line = reader.readLine())!=null){
@@ -175,17 +167,17 @@ public class CartController {
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
-                    orderBookPrice.setText(String.valueOf(Long.valueOf(b[finalI])*Long.valueOf(c[finalI])));
+                    orderBookPrice.setText(String.valueOf(Long.valueOf(orderBooksQuantity[finalI])*Long.valueOf(orderBooksPrice[finalI])));
                 }
             });
             reduce.setOnAction(new EventHandler<ActionEvent>() {
                 @Override
                 public void handle(ActionEvent actionEvent) {
                     if (Long.parseLong(orderBookQuantity.getText())>1){
-                        number = Long.valueOf(b[finalI]);
+                        number = Long.valueOf(orderBooksQuantity[finalI]);
                         number--;
                         orderBookQuantity.setText(String.valueOf(number));
-                        b[finalI] = String.valueOf(number);
+                        orderBooksQuantity[finalI] = String.valueOf(number);
                         try {
                             BufferedReader reader = new BufferedReader(new FileReader("Files/Cart.txt"));
                             StringBuilder lines = new StringBuilder();
@@ -194,8 +186,8 @@ public class CartController {
                             while (((line = reader.readLine())!=null) && (!String.valueOf(SignInController.loggedInUserId).equals(line.split("#")[1]))){
                                 lines.append(line + "\n");
                             }
-                            for (int j=0 ; j<(line.split("#").length-1)/2 ; j++){
-                                line1.append(d[j]+ "#" + b[j] + "#");
+                            for (int j=0 ; j<(line.split("#").length-2)/2 ; j++){
+                                line1.append(orderBooksID[j]+ "#" + orderBooksQuantity[j] + "#");
                             }
                             lines.append(line1 + "\n");
                             while ((line = reader.readLine())!=null){
@@ -210,19 +202,19 @@ public class CartController {
                         } catch (IOException e) {
                             throw new RuntimeException(e);
                         }
-                        orderBookPrice.setText(String.valueOf(Long.valueOf(b[finalI])*Long.valueOf(c[finalI])));
+                        orderBookPrice.setText(String.valueOf(Long.parseLong(orderBooksQuantity[finalI])*Long.parseLong(orderBooksPrice[finalI])));
                     }
                 }
             });
             delete.setOnAction(new EventHandler<ActionEvent>() {
                 @Override
                 public void handle(ActionEvent actionEvent) {
-                    newPane.getChildren().remove(orderHBox);
-                    for (int j = finalI ; j<a.length-1 ; j++){
-                        a[j] = a[j+1];
-                        b[j] = b[j+1];
-                        c[j] = c[j+1];
-                        d[j] = d[j+1];
+                    int lengh = orderBooksName.length-1;
+                    for (int j = finalI ; j<lengh ; j++){
+                        orderBooksName[j] = orderBooksName[j+1];
+                        orderBooksQuantity[j] = orderBooksQuantity[j+1];
+                        orderBooksPrice[j] = orderBooksPrice[j+1];
+                        orderBooksID[j] = orderBooksID[j+1];
                     }
                     try {
                         BufferedReader reader = new BufferedReader(new FileReader("Files/Cart.txt"));
@@ -233,7 +225,7 @@ public class CartController {
                             lines.append(line + "\n");
                         }
                         for (int k=0 ; k<(line.split("#").length-3)/2 ; k++){
-                            line1.append(d[k]+ "#" + b[k] + "#");
+                            line1.append(orderBooksID[k]+ "#" + orderBooksQuantity[k] + "#");
                         }
                         lines.append(line1 + "\n");
                         while ((line = reader.readLine())!=null){
@@ -248,8 +240,44 @@ public class CartController {
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
+                    try {
+                        ordersVBox.getChildren().clear();
+                        initialize();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    } catch (NoSuchFieldException e) {
+                        throw new RuntimeException(e);
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
             });
+        }
+    }
+    public void update() throws IOException {
+        String[] orderBooksID = new String[48];
+        String[] orderBooksQuantity = new String[48];
+        String[] orderBooksName= new String[48];
+        String[] orderBooksPrice = new String[48];
+        if (SignInController.loggedInUserId!=100) {
+            BufferedReader reader = new BufferedReader(new FileReader("Files/Cart.txt"));
+            String read;
+            while (((read = reader.readLine()) != null) && (!Objects.equals(read.split("#")[1], String.valueOf(SignInController.loggedInUserId)))) {
+            }
+            for (int i = 0; i < (read.split("#").length - 1) / 2; i++) {
+                int k = (2 * i) + 2;
+                orderBooksID[i] = read.split("#")[k];
+                orderBooksQuantity[i] = read.split("#")[k + 1];
+            }
+            reader.close();
+            for (int i = 0; i < 48 && orderBooksID[i] != null; i++) {
+                BufferedReader reader1 = new BufferedReader(new FileReader("Files/Books.txt"));
+                String line1;
+                while (((line1 = reader1.readLine()) != null) && (!Objects.equals(line1.split("#")[1], orderBooksID[i]))) {
+                }
+                orderBooksName[i] = line1.split("#")[4];
+                orderBooksPrice[i] = line1.split("#")[3];
+            }
         }
     }
 }
