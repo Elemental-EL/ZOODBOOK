a94571dd676e8612fa2746f24f0d465aeb8086a6
Mattio5 <mahdigamer1383sh@gmail.com>
2024-01-26 17:56:09 +0330

    uhu

--- src/main/java/org/example/zoodbook/ProfileController.java
+++ src/main/java/org/example/zoodbook/ProfileController.java
@@ -57,7 +57,11 @@ public class ProfileController {
         reader.close();
     }
     public void onBackClicked(MouseEvent event) throws IOException {
-
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
     public void onLogoClicked(MouseEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
