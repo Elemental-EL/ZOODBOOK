112161bd117cef4aececce61b641ac9524e44855
hamed005 <hamednobakht949@gmail.com>
2024-02-04 21:26:06 +0330

    update-10.2.1

--- src/main/java/org/example/zoodbook/AdminPanelController.java
+++ src/main/java/org/example/zoodbook/AdminPanelController.java
@@ -1,4 +1,25 @@
 package org.example.zoodbook;
 
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
 public class AdminPanelController {
+    public void onLogoClicked(MouseEvent event) {
+    }
+
+    public void onSeeUsersClicked(ActionEvent event) {
+    }
+
+    public void onStorageClicked(ActionEvent event) {
+    }
+
+    public void onSuggestionClicked(ActionEvent event) {
+    }
+
+    public void onOrdersClicked(ActionEvent event){
+
+    }
+
+    public void onLogOutClicked(ActionEvent event) {
+    }
 }
